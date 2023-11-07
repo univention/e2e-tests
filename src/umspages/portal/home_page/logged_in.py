b2b3311526c23e55318e684bc82028612cd265ad
Tobias Heinzmann <heinzmann.extern@univention.de>
2023-11-07 14:44:22 +0100

    chore: add missing tiles to logged_in page

--- src/umspages/portal/home_page/logged_in.py
+++ src/umspages/portal/home_page/logged_in.py
@@ -42,14 +42,21 @@ class HomePageLoggedIn(HomePage):
         super().set_content(*args, **kwargs)
         self.umc_heading = self.page.get_by_text("Univention Management Console", exact=True)
         self.users_tile = self.page.get_by_role("link", name=re.compile("User New Tab|Users iFrame"))
-        self.files_tile = self.page.get_by_role("link", name="Files New Tab")
+
         self.mail_tile = self.page.get_by_role("link", name="E-Mail New Tab")
         self.calendar_tile = self.page.get_by_role("link", name="Calendar New Tab")
         self.contacts_tile = self.page.get_by_role("link", name="Contacts New Tab")
         self.tasks_tile = self.page.get_by_role("link", name="Tasks New Tab")
-        self.video_conference_tile = self.page.get_by_role("link", name="Ad hoc videoconference New Tab")
-        self.projects_tile = self.page.get_by_role("link", name="Projects New Tab")
+
+        self.files_tile = self.page.get_by_role("link", name="Files New Tab")
         self.activity_tile = self.page.get_by_role("link", name="Activity New Tab")
+        self.new_files_folder_tile = self.page.get_by_role("button", name="Create new files Folder", exact=False)
+
+        self.projects_tile = self.page.get_by_role("link", name="Projects New Tab")
+        self.knowledge_tile = self.page.get_by_role("link",name="Knowledge New Tab")
+
+        self.collaboration_tile = self.page.get_by_role("link",name="Collaboration New Tab")
+        self.video_conference_tile = self.page.get_by_role("link", name="Ad hoc videoconference New Tab")
 
     def navigate(self, username, password):
         self.page.goto("/")
