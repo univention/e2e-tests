e6a182d6dede13a646b16d34be5ab1286d4c7dec
Johannes Bornhold <johannes.bornhold.extern@univention.de>
2023-12-06 16:27:10 +0100

    refactor: Move entry data access of navigation api into function
    For now using just a simple function since we have only two check of the
    navigation API. Otherwise the introduction of an abstraction object would have
    been the way to go.

--- tests/portal/test_central_navigation.py
+++ tests/portal/test_central_navigation.py
@@ -15,13 +15,19 @@ def test_navigation_api_returns_data_for_anonymous_user(navigation_api_url):
     data = response.json()
 
     assert response.status_code == requests.codes.ok
-    assert data["categories"][0]["entries"][0]["display_name"] == "Login"
+    display_name = _get_first_entry(data)["display_name"]
+    assert display_name == "Login"
 
 
 def test_navigation_api_returns_valid_icon_urls(navigation_api_url):
     response = requests.get(navigation_api_url)
     data = response.json()
-    icon_url = data["categories"][0]["entries"][0]["icon_url"]
+    icon_url = _get_first_entry(data)["icon_url"]
 
     response = requests.get(icon_url)
     assert response.status_code == requests.codes.ok
+
+
+def _get_first_entry(data):
+    first_entry = data["categories"][0]["entries"][0]
+    return first_entry
