5986b04d813dc4596db2408e2e3bd1587d489fdc
Johannes Bornhold <johannes.bornhold.extern@univention.de>
2023-12-06 16:24:48 +0100

    refactor: Move knowledge about API url into the fixture "navigation_api_url"

--- tests/portal/test_central_navigation.py
+++ tests/portal/test_central_navigation.py
@@ -1,9 +1,16 @@
+from urllib.parse import urljoin
+
 import requests
+import pytest
 
 
-def test_navigation_api_returns_data_for_anonymous_user():
-    navigation_api_url = "https://portal.uv-jbornhold.gaia.open-desk.cloud/univention/portal/navigation.json"
+@pytest.fixture
+def navigation_api_url(portal_base_url):
+    """URL of the navigation API in the Portal."""
+    return urljoin(portal_base_url, "/univention/portal/navigation.json")
 
+
+def test_navigation_api_returns_data_for_anonymous_user(navigation_api_url):
     response = requests.get(navigation_api_url)
     data = response.json()
 
@@ -11,9 +18,7 @@ def test_navigation_api_returns_data_for_anonymous_user():
     assert data["categories"][0]["entries"][0]["display_name"] == "Login"
 
 
-def test_navigation_api_returns_valid_icon_urls():
-    navigation_api_url = "https://portal.uv-jbornhold.gaia.open-desk.cloud/univention/portal/navigation.json"
-
+def test_navigation_api_returns_valid_icon_urls(navigation_api_url):
     response = requests.get(navigation_api_url)
     data = response.json()
     icon_url = data["categories"][0]["entries"][0]["icon_url"]
