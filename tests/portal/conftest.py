d9433cac5ae3be7c7af843711ede639807e0e855
Johannes Bornhold <johannes.bornhold.extern@univention.de>
2024-08-20 00:57:21 +0200

    feat: Pre-configure the browser context with cookie consent already given

--- tests/portal/conftest.py
+++ tests/portal/conftest.py
@@ -28,7 +28,8 @@
 # /usr/share/common-licenses/AGPL-3; if not, see
 # <https://www.gnu.org/licenses/>.
 
-from urllib.parse import urljoin
+from datetime import datetime, timedelta
+from urllib.parse import urljoin, urlparse
 
 import pytest
 import requests
@@ -62,12 +63,48 @@ def admin_password(pytestconfig):
 
 
 @pytest.fixture(scope="session")
-def browser_context_args(browser_context_args, portal_base_url):
+def browser_context_args(browser_context_args, portal_base_url, keycloak_base_url):
+    in_a_week = datetime.now() + timedelta(weeks=1)
+    portal_cookie_domain = urlparse(portal_base_url).hostname
+    keycloak_cookie_domain = urlparse(keycloak_base_url).hostname
     return {
         **browser_context_args,
         "base_url": portal_base_url,
         "locale": "en-US",
         "timezone_id": "Europe/Berlin",
+        "storage_state": {
+            # This is a slight violation of good end-to-end testing practices.
+            # Nearly all of our tests want to check the system with the cookie
+            # consent already given. This violation does make each of those
+            # tests simpler.
+            #
+            # Tests which explicitly need fresh cookies can use e.g.
+            # `pytest.mark.browser_context_args` or define this fixture in a
+            # smaller scope to adjust things.
+            'cookies': [
+                {
+                    'domain': f'.{keycloak_cookie_domain}',
+                    'expires': in_a_week.timestamp(),
+                    'httpOnly': False,
+                    'name': 'univentionCookieSettingsAccepted',
+                    'path': '/',
+                    'sameSite': 'Strict',
+                    'secure': False,
+                    'value': 'do-not-change-me',
+                },
+                {
+                    'domain': f'.{portal_cookie_domain}',
+                    'expires': in_a_week.timestamp(),
+                    'httpOnly': False,
+                    'name': 'univentionCookieSettingsAccepted',
+                    'path': '/',
+                    'sameSite': 'Strict',
+                    'secure': False,
+                    'value': 'do-not-change-me',
+                },
+            ],
+            'origins': [],
+        },
     }
 
 
