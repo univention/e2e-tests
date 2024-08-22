c5cca924450e1be82786cb641f9b85d1993c8d11
Johannes Bornhold <johannes.bornhold.extern@univention.de>
2024-08-22 10:06:22 +0200

    ci: Refactor entrypoint script to avoid duplication

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,27 @@
 #!/bin/sh
 
+: "${ADMIN_PASSWORD:=univention}"
+: "${ADMIN_USERNAME:=default.admin}"
+: "${MARKER:=development_environment}"
+: "${PORTAL_CENTRAL_NAVIGATION_SECRET:=univention}"
+: "${PORTAL_BASE_URL:=http://localhost:8000/univention/portal/}"
+: "${UDM_ADMIN_PASSWORD:=univention}"
+: "${UDM_ADMIN_USERNAME:=cn=admin}"
+: "${USER_PASSWORD:=univention}"
+: "${USER_USERNAME:=default.user}"
+
 # Check if PYTEST_ADDOPTS is already set
 if [ -z "$PYTEST_ADDOPTS" ]; then
     # Set PYTEST_ADDOPTS based on NAMESPACE presence
     if [ -n "$NAMESPACE" ]; then
-        export PYTEST_ADDOPTS="-m acceptance_environment --admin-username default.admin --admin-password $ADMIN_PASSWORD --username default.user --password $USER_PASSWORD --udm-admin-username cn=admin --udm-admin-password $UDM_ADMIN_PASSWORD --portal-central-navigation-secret $PORTAL_CENTRAL_NAVIGATION_SECRET --num-ip-block 7 --release-duration 1 --portal-base-url https://portal.$NAMESPACE.gaia.open-desk.cloud/univention/portal/"
-    # else if PORTAL_BASE_URL
+        MARKER="acceptance_environment"
+        PORTAL_BASE_URL="https://portal.$NAMESPACE.gaia.open-desk.cloud/univention/portal/"
+        # else if PORTAL_BASE_URL
     elif [ -n "$PORTAL_BASE_URL" ]; then
-        export PYTEST_ADDOPTS="-m acceptance_environment --admin-username default.admin --admin-password $ADMIN_PASSWORD --username default.user --password $USER_PASSWORD --udm-admin-username cn=admin --udm-admin-password $UDM_ADMIN_PASSWORD --portal-central-navigation-secret $PORTAL_CENTRAL_NAVIGATION_SECRET --num-ip-block 7 --release-duration 1 --portal-base-url $PORTAL_BASE_URL"
-    else
-        export PYTEST_ADDOPTS="-m development_environment --admin-username default.admin --admin-password univention --username default.user --password univention --udm-admin-username cn=admin --udm-admin-password univention --portal-central-navigation-secret univention --num-ip-block 7 --release-duration 1 --portal-base-url http://localhost:8000/univention/portal/"
+        MARKER="acceptance_environment"
     fi
+
+    export PYTEST_ADDOPTS="-m ${MARKER} --admin-username ${ADMIN_USERNAME} --admin-password ${ADMIN_PASSWORD} --username ${USER_USERNAME} --password ${USER_PASSWORD} --udm-admin-username ${UDM_ADMIN_USERNAME} --udm-admin-password ${UDM_ADMIN_PASSWORD} --portal-central-navigation-secret ${PORTAL_CENTRAL_NAVIGATION_SECRET} --num-ip-block 7 --release-duration 1 --portal-base-url ${PORTAL_BASE_URL}"
 fi
 
 exec "$@"
