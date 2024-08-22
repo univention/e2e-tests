809dc34b672c3e7f6ec9fec27a9e9291416d35e8
Johannes Bornhold <johannes.bornhold.extern@univention.de>
2024-08-22 10:09:58 +0200

    ci: Add maildev api configuration options into entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,6 +9,9 @@
 : "${UDM_ADMIN_USERNAME:=cn=admin}"
 : "${USER_PASSWORD:=univention}"
 : "${USER_USERNAME:=default.user}"
+: "${EMAIL_TEST_API_USERNAME:=user}"
+: "${EMAIL_TEST_API_PASSWORD:=password}"
+: "${EMAIL_TEST_API_BASE_URL:=http://localhost:8001/email}"
 
 # Check if PYTEST_ADDOPTS is already set
 if [ -z "$PYTEST_ADDOPTS" ]; then
@@ -21,7 +24,7 @@ if [ -z "$PYTEST_ADDOPTS" ]; then
         MARKER="acceptance_environment"
     fi
 
-    export PYTEST_ADDOPTS="-m ${MARKER} --admin-username ${ADMIN_USERNAME} --admin-password ${ADMIN_PASSWORD} --username ${USER_USERNAME} --password ${USER_PASSWORD} --udm-admin-username ${UDM_ADMIN_USERNAME} --udm-admin-password ${UDM_ADMIN_PASSWORD} --portal-central-navigation-secret ${PORTAL_CENTRAL_NAVIGATION_SECRET} --num-ip-block 7 --release-duration 1 --portal-base-url ${PORTAL_BASE_URL}"
+    export PYTEST_ADDOPTS="-m ${MARKER} --admin-username ${ADMIN_USERNAME} --admin-password ${ADMIN_PASSWORD} --username ${USER_USERNAME} --password ${USER_PASSWORD} --udm-admin-username ${UDM_ADMIN_USERNAME} --udm-admin-password ${UDM_ADMIN_PASSWORD} --portal-central-navigation-secret ${PORTAL_CENTRAL_NAVIGATION_SECRET} --num-ip-block 7 --release-duration 1 --portal-base-url ${PORTAL_BASE_URL} --email-test-api-username=${EMAIL_TEST_API_USERNAME} --email-test-api-password=${EMAIL_TEST_API_PASSWORD} --email-test-api-base-url=${EMAIL_TEST_API_BASE_URL}"
 fi
 
 exec "$@"
