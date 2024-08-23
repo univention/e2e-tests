7c2a87fbfd9c74583f04a8873a4ac99aa754685a
Johannes Bornhold <johannes.bornhold.extern@univention.de>
2024-08-23 08:59:15 +0200

    feat: Allow a custom release name in discover script

--- discover-env-from-cluster.sh
+++ discover-env-from-cluster.sh
@@ -9,11 +9,14 @@
 #    Regarding the namespace, either use "kubens" or set the variable
 #    "DEPLOY_NAMESPACE".
 #
-# 2. Source this file to set the environment variables:
+# 2. Adjust the release name if it is not "nubus" by setting the variable
+#    "RELEASE_NAME".
+#
+# 3. Source this file to set the environment variables:
 #
 #        source discover-env-from-cluster.sh
 #
-# 3. Check the output.
+# 4. Check the output.
 
 
 # If no namespace is provided, then try to discover the namespace via "kubectl".
@@ -21,7 +24,13 @@
 # configuration related to the currently selected context.
 : "${DEPLOY_NAMESPACE:=$(kubectl config view --minify -o jsonpath='{..namespace}')}"
 
+# The release name is typically "nubus". In openDesk it is currently "ums". In
+# general it can be freely chosen.
+: "${RELEASE_NAME:=nubus}"
+
+
 echo "Namespace: ${DEPLOY_NAMESPACE}"
+echo "Release name: ${RELEASE_NAME}"
 echo "API Server: $(kubectl config view --minify -o jsonpath='{..server}')"
 echo "Cluster: $(kubectl config view --minify -o jsonpath='{.contexts[0].context.cluster}')"
 
@@ -30,14 +39,14 @@ echo "Cluster: $(kubectl config view --minify -o jsonpath='{.contexts[0].context
 #
 # These commands depend on the Nubus deployment and will have to be kept in sync
 # with the progressing Nubus chart.
-default_user_password=$(kubectl get secret -n "${DEPLOY_NAMESPACE}" nubus-nubus-credentials -o jsonpath="{.data.user_password}" | base64 -d)
-default_admin_password=$(kubectl get secret -n "${DEPLOY_NAMESPACE}" nubus-nubus-credentials -o jsonpath="{.data.admin_password}" | base64 -d)
-udm_admin_password=$(kubectl get secret -n "${DEPLOY_NAMESPACE}" nubus-udm-rest-api-credentials -o jsonpath="{.data['machine\.secret']}" | base64 -d)
-portal_base_url=https://$(kubectl get ingress -n "${DEPLOY_NAMESPACE}" nubus-portal-server -o jsonpath="{.spec.rules[0].host}")
-keycloak_base_url=https://$(kubectl get ingress -n "${DEPLOY_NAMESPACE}" nubus-keycloak-extensions-proxy -o jsonpath="{.spec.rules[0].host}")
+default_user_password=$(kubectl get secret -n "${DEPLOY_NAMESPACE}" "${RELEASE_NAME}-nubus-credentials" -o jsonpath="{.data.user_password}" | base64 -d)
+default_admin_password=$(kubectl get secret -n "${DEPLOY_NAMESPACE}" "${RELEASE_NAME}-nubus-credentials" -o jsonpath="{.data.admin_password}" | base64 -d)
+udm_admin_password=$(kubectl get secret -n "${DEPLOY_NAMESPACE}" "${RELEASE_NAME}-udm-rest-api-credentials" -o jsonpath="{.data['machine\.secret']}" | base64 -d)
+portal_base_url=https://$(kubectl get ingress -n "${DEPLOY_NAMESPACE}" "${RELEASE_NAME}-portal-server" -o jsonpath="{.spec.rules[0].host}")
+keycloak_base_url=https://$(kubectl get ingress -n "${DEPLOY_NAMESPACE}" "${RELEASE_NAME}-keycloak-extensions-proxy" -o jsonpath="{.spec.rules[0].host}")
 email_test_api_base_url=https://$(kubectl get ingress -n "${DEPLOY_NAMESPACE}" maildev -o jsonpath="{.spec.rules[0].host}")
 email_test_api_password=$(kubectl get secret -n "${DEPLOY_NAMESPACE}" maildev-web -o jsonpath="{.data.web-password}" | base64 -d)
-portal_central_navigation_secret=$(kubectl get secret -n "${DEPLOY_NAMESPACE}" nubus-portal-server-central-navigation-shared-secret -o jsonpath="{.data['authenticator\.secret']}" | base64 -d)
+portal_central_navigation_secret=$(kubectl get secret -n "${DEPLOY_NAMESPACE}" "${RELEASE_NAME}-portal-server-central-navigation-shared-secret" -o jsonpath="{.data['authenticator\.secret']}" | base64 -d)
 
 
 export PYTEST_ADDOPTS="--portal-base-url=${portal_base_url} --username=default.user --password=${default_user_password} --udm-admin-username='cn=admin' --udm-admin-password=${udm_admin_password} --admin-username=default.admin --admin-password=${default_admin_password} --email-test-api-username=user --email-test-api-password=${email_test_api_password} --email-test-api-base-url=${email_test_api_base_url} --portal-central-navigation-secret=${portal_central_navigation_secret} --keycloak-base-url=${keycloak_base_url}"
