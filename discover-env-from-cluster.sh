univention/e2e-tests

commit c41869bc85a8786862e1c98265eac2f5619291fb

feat: remove default.user and admin for Administrator

discover-env-from-cluster.sh
@@ -42,11 +42,12 @@ echo "Cluster: $(kubectl config view --minify -o jsonpath='{.contexts[0].context
 #
 # These commands depend on the Nubus deployment and will have to be kept in sync
 # with the progressing Nubus chart.
-default_user_password=$(kubectl get secret -n "${DEPLOY_NAMESPACE}" "${RELEASE_NAME}-nubus-credentials" -o jsonpath="{.data.user_password}" | base64 -d)
-default_admin_password=$(kubectl get secret -n "${DEPLOY_NAMESPACE}" "${RELEASE_NAME}-nubus-credentials" -o jsonpath="{.data.admin_password}" | base64 -d)
-udm_admin_password=$(kubectl get secret -n "${DEPLOY_NAMESPACE}" "${RELEASE_NAME}-udm-rest-api-credentials" -o jsonpath="{.data['machine\.secret']}" | base64 -d)
-portal_base_url=https://$(kubectl get ingress -n "${DEPLOY_NAMESPACE}" "${RELEASE_NAME}-portal-server" -o jsonpath="{.spec.rules[0].host}")
-keycloak_base_url=https://$(kubectl get ingress -n "${DEPLOY_NAMESPACE}" "${RELEASE_NAME}-keycloak-extensions-proxy" -o jsonpath="{.spec.rules[0].host}")
+default_admin_password=$(kubectl get secret -n "${DEPLOY_NAMESPACE}" nubus-nubus-credentials -o jsonpath="{.data.administrator_password}" | base64 -d)
+portal_base_url=https://$(kubectl get ingress -n "${DEPLOY_NAMESPACE}" nubus-portal-server -o jsonpath="{.spec.rules[0].host}")
+keycloak_base_url=https://$(kubectl get ingress -n "${DEPLOY_NAMESPACE}" nubus-keycloak-extensions-proxy -o jsonpath="{.spec.rules[0].host}")
+email_test_api_base_url=https://$(kubectl get ingress -n "${DEPLOY_NAMESPACE}" maildev -o jsonpath="{.spec.rules[0].host}")
+email_test_api_password=$(kubectl get secret -n "${DEPLOY_NAMESPACE}" maildev-web -o jsonpath="{.data.web-password}" | base64 -d)
+portal_central_navigation_secret=$(kubectl get secret -n "${DEPLOY_NAMESPACE}" nubus-portal-server-central-navigation-shared-secret -o jsonpath="{.data['authenticator\.secret']}" | base64 -d)
 
 email_test_api_base_url=$(kubectl get ingress -n "${DEPLOY_NAMESPACE}" maildev -o jsonpath="{.spec.rules[0].host}")
 if [ -n "$email_test_api_base_url" ]
@@ -67,7 +68,7 @@ else
     portal_central_navigation_secret="${the_usual_portal_central_navigation_secret}"
 fi
 
-export PYTEST_ADDOPTS="--portal-base-url=${portal_base_url} --username=default.user --password=${default_user_password} --udm-admin-username='cn=admin' --udm-admin-password=${udm_admin_password} --admin-username=default.admin --admin-password=${default_admin_password} --email-test-api-username=user --email-test-api-password=${email_test_api_password} --email-test-api-base-url=${email_test_api_base_url} --portal-central-navigation-secret=${portal_central_navigation_secret} --keycloak-base-url=${keycloak_base_url}"
+export PYTEST_ADDOPTS="--portal-base-url=${portal_base_url} --admin-password=${default_admin_password} --email-test-api-username=user --email-test-api-password=${email_test_api_password} --email-test-api-base-url=${email_test_api_base_url} --portal-central-navigation-secret=${portal_central_navigation_secret} --keycloak-base-url=${keycloak_base_url}"
 
 
 echo

entrypoint.sh
@@ -4,14 +4,10 @@
 
 
 : "${ADMIN_PASSWORD:=univention}"
-: "${ADMIN_USERNAME:=default.admin}"
+: "${ADMIN_USERNAME:=Administrator}"
 : "${MARKER:=development_environment}"
 : "${PORTAL_CENTRAL_NAVIGATION_SECRET:=univention}"
 : "${PORTAL_BASE_URL:=http://localhost:8000/univention/portal/}"
-: "${UDM_ADMIN_PASSWORD:=univention}"
-: "${UDM_ADMIN_USERNAME:=cn=admin}"
-: "${USER_PASSWORD:=univention}"
-: "${USER_USERNAME:=default.user}"
 : "${EMAIL_TEST_API_USERNAME:=user}"
 : "${EMAIL_TEST_API_PASSWORD:=password}"
 : "${EMAIL_TEST_API_BASE_URL:=http://localhost:8001/email}"
@@ -27,7 +23,7 @@ if [ -z "$PYTEST_ADDOPTS" ]; then
         MARKER="acceptance_environment"
     fi
 
-    export PYTEST_ADDOPTS="-m ${MARKER} --admin-username ${ADMIN_USERNAME} --admin-password ${ADMIN_PASSWORD} --username ${USER_USERNAME} --password ${USER_PASSWORD} --udm-admin-username ${UDM_ADMIN_USERNAME} --udm-admin-password ${UDM_ADMIN_PASSWORD} --portal-central-navigation-secret ${PORTAL_CENTRAL_NAVIGATION_SECRET} --num-ip-block 7 --release-duration 1 --portal-base-url ${PORTAL_BASE_URL} --email-test-api-username=${EMAIL_TEST_API_USERNAME} --email-test-api-password=${EMAIL_TEST_API_PASSWORD} --email-test-api-base-url=${EMAIL_TEST_API_BASE_URL}"
+    export PYTEST_ADDOPTS="-m ${MARKER} --admin-password ${ADMIN_PASSWORD} --portal-central-navigation-secret ${PORTAL_CENTRAL_NAVIGATION_SECRET} --num-ip-block 7 --release-duration 1 --portal-base-url ${PORTAL_BASE_URL} --email-test-api-username=${EMAIL_TEST_API_USERNAME} --email-test-api-password=${EMAIL_TEST_API_PASSWORD} --email-test-api-base-url=${EMAIL_TEST_API_BASE_URL}"
 fi
 
 # NOTE: This will show secrets in the test logs.

src/umspages/portal/selfservice/set_recovery_email.py
@@ -30,6 +30,9 @@
 
 import re
 
+# from playwright._impl._api_types import TimeoutError
+from playwright.sync_api import TimeoutError
+
 from ...common.base import BasePage
 from ..home_page.logged_in import HomePageLoggedIn
 
@@ -47,11 +50,16 @@ class SetRecoveryEmailDialogPage(BasePage):
     def navigate(self, username, password):
         home_page_logged_in = HomePageLoggedIn(self.page)
         home_page_logged_in.navigate(username, password)
-        home_page_logged_in.reveal_area(
-            home_page_logged_in.right_side_menu,
-            home_page_logged_in.header.hamburger_icon,
-        )
-        home_page_logged_in.right_side_menu.click_entry("User settings")
+        for _ in range(3):
+            try:
+                home_page_logged_in.reveal_area(
+                    home_page_logged_in.right_side_menu,
+                    home_page_logged_in.header.hamburger_icon,
+                )
+                home_page_logged_in.right_side_menu.click_entry("User settings")
+                break
+            except TimeoutError:
+                self.page.reload()
         home_page_logged_in.right_side_menu.click_sub_entry(re.compile("My password recovery options"))
         self.username_box.fill(username)
         self.password_box.fill(password)

tests/conftest.py
@@ -41,27 +41,11 @@ from univention.admin.rest.client import UDM
 def pytest_addoption(parser):
     # Portal tests options
     parser.addoption("--portal-base-url", help="Base URL of the univention portal")
-    parser.addoption("--username", help="Portal login username")
-    parser.addoption("--password", help="Portal login password")
-    parser.addoption("--admin-username", help="Portal admin login username")
-    parser.addoption("--admin-password", help="Portal admin login password")
-    parser.addoption("--udm-admin-username", default="cn=admin", help="UDM admin login password")
-    parser.addoption("--udm-admin-password", default="univention", help="UDM admin login password")
-    parser.addoption(
-        "--email-test-api-username",
-        default="user",
-        help="Username to access the email test API.",
-    )
-    parser.addoption(
-        "--email-test-api-password",
-        default="univention",
-        help="Password to access the email test API.",
-    )
-    parser.addoption(
-        "--email-test-api-base-url",
-        default="",
-        help="Base URL to reach the email test API (Maildev).",
-    )
+    parser.addoption("--admin-username", default="Administrator", help="Portal admin login username")
+    parser.addoption("--admin-password", default="univention", help="Portal admin login password")
+    parser.addoption("--email-test-api-username", default="user", help="Username to access the email test API.")
+    parser.addoption("--email-test-api-password", default="univention", help="Password to access the email test API.")
+    parser.addoption("--email-test-api-base-url", default="", help="Base URL to reach the email test API (Maildev).")
     parser.addoption(
         "--portal-central-navigation-secret",
         default="univention",
@@ -70,41 +54,22 @@ def pytest_addoption(parser):
     # BFP tests options
     parser.addoption("--keycloak-base-url", help="Base URL of Keycloak")
     parser.addoption("--kc-admin-username", default="admin", help="Keycloak admin login username")
-    parser.addoption(
-        "--kc-admin-password",
-        default="univention",
-        help="Keycloak admin login password",
-    )
-    parser.addoption(
-        "--num-device-block",
-        type=int,
-        default=5,
-        help="Number of failed logins for device block",
-    )
-    parser.addoption(
-        "--num-ip-block",
-        type=int,
-        default=7,
-        help="Number of failed logins for IP block",
-    )
-    parser.addoption(
-        "--release-duration",
-        type=int,
-        default=1,
-        help="Blocks are released after this many minutes",
-    )
+    parser.addoption("--kc-admin-password", default="univention", help="Keycloak admin login password")
+    parser.addoption("--num-device-block", type=int, default=5, help="Number of failed logins for device block")
+    parser.addoption("--num-ip-block", type=int, default=7, help="Number of failed logins for IP block")
+    parser.addoption("--release-duration", type=int, default=1, help="Blocks are released after this many minutes")
     parser.addoption("--realm", default="master", help="Realm to attempt logins at")
     parser.addoption("--randomly-seed", help="Seed to use for randomization.")
 
 
 @pytest.fixture(scope="session")
-def udm_admin_username(pytestconfig):
-    return pytestconfig.option.udm_admin_username
+def admin_username(pytestconfig):
+    return pytestconfig.option.admin_username
 
 
 @pytest.fixture(scope="session")
-def udm_admin_password(pytestconfig):
-    return pytestconfig.option.udm_admin_password
+def admin_password(pytestconfig):
+    return pytestconfig.option.admin_password
 
 
 @pytest.fixture(scope="session")
@@ -192,11 +157,11 @@ def udm_rest_api_base_url(portal_base_url):
 
 
 @pytest.fixture(scope="session")
-def udm(udm_rest_api_base_url, udm_admin_username, udm_admin_password):
+def udm(udm_rest_api_base_url, admin_username, admin_password):
     """
     A configured instance of the UDM Rest API client.
     """
-    udm = UDM(udm_rest_api_base_url, udm_admin_username, udm_admin_password)
+    udm = UDM(udm_rest_api_base_url, admin_username, admin_password)
     _verify_udm_rest_api_configuration(udm)
     return udm
 

tests/portal/conftest.py
@@ -43,26 +43,6 @@ from umspages.portal.selfservice.logged_out import SelfservicePortalLoggedOut
 
 
 @pytest.fixture(scope="session")
-def username(pytestconfig):
-    return pytestconfig.option.username
-
-
-@pytest.fixture(scope="session")
-def password(pytestconfig):
-    return pytestconfig.option.password
-
-
-@pytest.fixture(scope="session")
-def admin_username(pytestconfig):
-    return pytestconfig.option.admin_username
-
-
-@pytest.fixture(scope="session")
-def admin_password(pytestconfig):
-    return pytestconfig.option.admin_password
-
-
-@pytest.fixture(scope="session")
 def browser_context_args(browser_context_args, portal_base_url, keycloak_base_url):
     in_a_week = datetime.now() + timedelta(weeks=1)
     portal_cookie_domain = urlparse(portal_base_url).hostname
@@ -137,16 +117,16 @@ def navigate_to_saml_login_page(page):
 
 
 @pytest.fixture()
-def navigate_to_home_page_logged_in(page, username, password):
+def navigate_to_home_page_logged_in(page, admin_username, admin_password):
     home_page_logged_in = HomePageLoggedIn(page)
-    home_page_logged_in.navigate(username, password)
+    home_page_logged_in.navigate(admin_username, admin_password)
     return page
 
 
 @pytest.fixture()
-def navigate_to_selfservice_portal_logged_in(page, username, password):
+def navigate_to_selfservice_portal_logged_in(page, admin_username, admin_password):
     selfservice_portal_logged_in = SelfservicePortalLoggedIn(page)
-    selfservice_portal_logged_in.navigate(username, password)
+    selfservice_portal_logged_in.navigate(admin_username, admin_password)
     return page
 
 
@@ -158,7 +138,7 @@ def navigate_to_home_page_logged_in_as_admin(page, admin_username, admin_passwor
 
 
 @pytest.fixture(scope="session")
-def udm_session(udm_admin_username, udm_admin_password):
+def udm_session(admin_username, admin_password):
     """
     Prepares an instance of `requests.Session` to call the UDM Rest API.
 
@@ -166,7 +146,7 @@ def udm_session(udm_admin_username, udm_admin_password):
     requests will run with full permissions in the UDM Rest API.
     """
     udm_session = requests.Session()
-    udm_session.auth = (udm_admin_username, udm_admin_password)
+    udm_session.auth = (admin_username, admin_password)
     udm_session.headers.update({"accept": "application/json"})
     return udm_session
 

tests/portal/test_brute_force_protection.py
@@ -40,8 +40,8 @@ from umspages.portal.login_page import LoginPage
 @pytest.mark.acceptance_environment
 def test_portal_login_ip_block(
     navigate_to_login_page,
-    username,
-    password,
+    admin_username,
+    admin_password,
     failed_attempts_before_ip_block,
     block_expiration_duration,
     pytestconfig,
@@ -64,12 +64,12 @@ def test_portal_login_ip_block(
     page = navigate_to_login_page
     login_page = LoginPage(page)
     for _ in range(failed_attempts_before_ip_block):
-        login_page.login(username, f"{password}_wrong_password")
+        login_page.login(admin_username, f"{admin_password}_wrong_password")
         expect(login_page.page.get_by_text("Invalid username or password.")).to_be_visible(timeout=4000)
     # keycloak-extensions handler processes all the failed login events every
     # two seconds
     page.wait_for_timeout(2200)
-    login_page.login(username, f"{password}_wrong_password")
+    login_page.login(admin_username, f"{admin_password}_wrong_password")
     ip_block_page = OnIPBlockPage(page)
     ip_block_page.is_displayed()
 
@@ -77,6 +77,6 @@ def test_portal_login_ip_block(
     page.wait_for_timeout(round(block_expiration_duration * 60 * 1000) + 1)
     login_page = LoginPage(page)
     login_page.navigate()
-    login_page.login(username, password)
+    login_page.login(admin_username, admin_password)
     home_page_logged_in = HomePageLoggedIn(page)
     home_page_logged_in.assert_logged_in()

tests/portal/test_central_navigation.py
@@ -44,9 +44,9 @@ def test_navigation_api_returns_valid_icon_urls(navigation_api_url):
 @pytest.mark.development_environment
 @pytest.mark.acceptance_environment
 def test_navigation_api_returns_data_for_authenticated_user(
-    navigation_api_url, username, portal_central_navigation_secret
+    navigation_api_url, admin_username, portal_central_navigation_secret
 ):
-    response = requests.get(navigation_api_url, auth=(username, portal_central_navigation_secret))
+    response = requests.get(navigation_api_url, auth=(admin_username, portal_central_navigation_secret))
     data = response.json()
 
     assert response.status_code == requests.codes.ok

tests/portal/test_cookies.py
@@ -35,13 +35,13 @@ from umspages.portal.login_page import LoginPage
 
 
 @pytest.fixture(scope="module")
-def logged_in_cookies(browser, browser_context_args, username, password):
+def logged_in_cookies(browser, browser_context_args, admin_username, admin_password):
     context = browser.new_context(**browser_context_args)
     page = context.new_page()
 
     login_page = LoginPage(page)
     login_page.navigate()
-    login_page.login(username, password)
+    login_page.login(admin_username, admin_password)
 
     home_page_logged_in = HomePageLoggedIn(page)
     home_page_logged_in.assert_logged_in()
@@ -64,7 +64,7 @@ def test_cookie_hardening_sets_samesite(logged_in_cookies):
 @pytest.mark.acceptance_environment
 def test_cookie_hardening_sets_secure(logged_in_cookies):
     umc_session_cookie = _get_cookie(logged_in_cookies, "UMCSessionId")
-    assert umc_session_cookie["secure"] == True  # noqa E712
+    assert umc_session_cookie["secure"] is True
 
 
 @pytest.mark.cookies

commit eb9a0c7dc70deda7aa06559e9325c53a4aaaac09

fix: Make support for "RELEASE_NAME" work again
See: c41869bc85a8786862e1c98265eac2f5619291fb introduced the issue accidentally

discover-env-from-cluster.sh
@@ -42,12 +42,9 @@ echo "Cluster: $(kubectl config view --minify -o jsonpath='{.contexts[0].context
 #
 # These commands depend on the Nubus deployment and will have to be kept in sync
 # with the progressing Nubus chart.
-default_admin_password=$(kubectl get secret -n "${DEPLOY_NAMESPACE}" nubus-nubus-credentials -o jsonpath="{.data.administrator_password}" | base64 -d)
-portal_base_url=https://$(kubectl get ingress -n "${DEPLOY_NAMESPACE}" nubus-portal-server -o jsonpath="{.spec.rules[0].host}")
-keycloak_base_url=https://$(kubectl get ingress -n "${DEPLOY_NAMESPACE}" nubus-keycloak-extensions-proxy -o jsonpath="{.spec.rules[0].host}")
-email_test_api_base_url=https://$(kubectl get ingress -n "${DEPLOY_NAMESPACE}" maildev -o jsonpath="{.spec.rules[0].host}")
-email_test_api_password=$(kubectl get secret -n "${DEPLOY_NAMESPACE}" maildev-web -o jsonpath="{.data.web-password}" | base64 -d)
-portal_central_navigation_secret=$(kubectl get secret -n "${DEPLOY_NAMESPACE}" nubus-portal-server-central-navigation-shared-secret -o jsonpath="{.data['authenticator\.secret']}" | base64 -d)
+default_admin_password=$(kubectl get secret -n "${DEPLOY_NAMESPACE}" "${RELEASE_NAME}-nubus-credentials" -o jsonpath="{.data.administrator_password}" | base64 -d)
+portal_base_url=https://$(kubectl get ingress -n "${DEPLOY_NAMESPACE}" "${RELEASE_NAME}-portal-server" -o jsonpath="{.spec.rules[0].host}")
+keycloak_base_url=https://$(kubectl get ingress -n "${DEPLOY_NAMESPACE}" "${RELEASE_NAME}-keycloak-extensions-proxy" -o jsonpath="{.spec.rules[0].host}")
 
 email_test_api_base_url=$(kubectl get ingress -n "${DEPLOY_NAMESPACE}" maildev -o jsonpath="{.spec.rules[0].host}")
 if [ -n "$email_test_api_base_url" ]