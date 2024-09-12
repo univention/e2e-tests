2169c12d23f519600cd7454934bfa14725585066
Johannes Lohmer <lohmer@univention.de>
2024-09-12 16:31:35 +0200

    fix(testing-api): make api available on the base route /testing-api/v1

--- testing-api/src/testing_api/main.py
+++ testing-api/src/testing_api/main.py
@@ -6,7 +6,7 @@ import socket
 import time
 
 import uvicorn
-from fastapi import FastAPI
+from fastapi import APIRouter, FastAPI
 
 from testing_api.config import get_testing_api_settings
 from testing_api.ldap_replication import check_replication_status
@@ -15,27 +15,27 @@ from testing_api.log import setup_logging
 logger = logging.getLogger(__name__)
 
 
-app = FastAPI()
-
-
 def resolve_pod_ips_from_headless_service(service_hostname: str) -> list[str]:
-
     address_info = socket.getaddrinfo(service_hostname, None)
     pod_ips = list(set((info[4][0] for info in address_info)))
     logger.debug("Resolved pod ips: %s from headless service hostname: %s.", pod_ips, service_hostname)
     return pod_ips
 
 
-@app.get("/ldap-replication-status")
+app = FastAPI()
+
+
+testing_api_v1_router = APIRouter(prefix="/testing-api/v1")
+
+
+@testing_api_v1_router.get("/ldap-replication-status")
 async def ldap_replication_status(timeout: float | int = 10):
     start = time.perf_counter()
     logger.debug("Ensuring that the replication is in sync between the ldap primary and secondary")
 
     settings = get_testing_api_settings()
 
-    ldap_secondary_ips = resolve_pod_ips_from_headless_service(
-        settings.ldap_server_secondary_service_hostname
-    )
+    ldap_secondary_ips = resolve_pod_ips_from_headless_service(settings.ldap_server_secondary_service_hostname)
     assert ldap_secondary_ips
     check_replication_status(timeout, ldap_secondary_ips, settings)
 
@@ -47,20 +47,23 @@ async def ldap_replication_status(timeout: float | int = 10):
     return True
 
 
+app.include_router(testing_api_v1_router)
+
+
 @app.on_event("startup")
 async def startup_task():
     logger.info("Started %s.", app.title)
+
+    for route in app.routes:
+        logger.debug("API route: %s", route)
+
     settings = get_testing_api_settings()
 
-    ldap_secondary_ips = resolve_pod_ips_from_headless_service(
-        settings.ldap_server_secondary_service_hostname
-    )
+    ldap_secondary_ips = resolve_pod_ips_from_headless_service(settings.ldap_server_secondary_service_hostname)
     assert ldap_secondary_ips
 
     check_replication_status(1, ldap_secondary_ips, settings)
     logger.info("LDAP Connections are workind and replication status can be monitored.")
-
-
 
 
 def main():
@@ -68,7 +71,7 @@ def main():
     assert settings
     setup_logging(settings.log_level)
 
-    uvicorn.run(app, host="127.0.0.1", port=4434, log_config=None)
+    uvicorn.run(app, host="0.0.0.0", port=4434, log_config=None)
 
 
 if __name__ == "__main__":
