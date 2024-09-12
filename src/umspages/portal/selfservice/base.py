cda150d53b178015085359ee079b0ca8ba36cd86
Johannes Bornhold <johannes.bornhold.extern@univention.de>
2024-09-12 15:36:00 +0200

    refactor: Remove SelfservicePortalLoggedOut in favor of SelfservicePortal

--- src/umspages/portal/selfservice/base.py
+++ src/umspages/portal/selfservice/base.py
@@ -19,13 +19,17 @@
 # /usr/share/common-licenses/AGPL-3; if not, see
 # <https://www.gnu.org/licenses/>.
 
+from ...common.base import expect
 from ..common.portal_page import PortalPage
+from ..login_page import LoginPage
 
 
 class SelfservicePortal(PortalPage):
     """
-    Common selfservice portal page stuff applicable for both logged in and
-    logged out state
+    The selfservice portal is a separate portal focused on selfservice.
+
+    It does provide functionality for password reset and also around account
+    management.
     """
 
     def navigate(self):
@@ -42,3 +46,13 @@ class SelfservicePortal(PortalPage):
         # UMC tiles headline - Expected not to be there in default
         # configurations
         self.umc_tiles_headline = self.page.get_by_role("heading", name="Univention Management Console")
+
+    def login(self, username, password):
+        self.reveal_area(self.right_side_menu, self.header.hamburger_icon)
+        expect(self.right_side_menu.login_button).to_be_visible()
+        expect(self.right_side_menu.logout_button).to_be_hidden()
+        self.right_side_menu.login_button.click(timeout=2000)
+
+        login_page = LoginPage(self.page)
+        login_page.login_with_retry(username, password)
+        self.page.wait_for_url("/univention/selfservice/**", timeout=5000)
--- tests/portal/test_selfservice.py
+++ tests/portal/test_selfservice.py
@@ -42,7 +42,6 @@ from umspages.portal.home_page.logged_out import HomePageLoggedOut
 from umspages.portal.login_page import LoginPage
 from umspages.portal.selfservice.base import SelfservicePortal
 from umspages.portal.selfservice.change_password import ChangePasswordDialogPage
-from umspages.portal.selfservice.logged_in import SelfservicePortalLoggedIn
 from umspages.portal.selfservice.manage_profile import ManageProfileDialogPage
 from umspages.portal.selfservice.password_forgotten import PasswordForgottenPage
 from umspages.portal.selfservice.set_new_password import SetNewPasswordPage
@@ -186,12 +185,12 @@ def test_manage_profile(user, user_password, wait_for_portal_sync: WaitForPortal
 @pytest.mark.development_environment
 @pytest.mark.acceptance_environment
 def test_admin_sees_correct_tiles_in_selfservice_portal(page, admin_username, admin_password):
-    selfservice_portal_logged_in = SelfservicePortalLoggedIn(page)
-    selfservice_portal_logged_in.navigate()
-    selfservice_portal_logged_in.login(admin_username, admin_password)
+    selfservice_portal = SelfservicePortal(page)
+    selfservice_portal.navigate()
+    selfservice_portal.login(admin_username, admin_password)
 
-    expect(selfservice_portal_logged_in.my_profile_tile).to_be_visible()
-    expect(selfservice_portal_logged_in.protect_account_tile).to_be_visible()
+    expect(selfservice_portal.my_profile_tile).to_be_visible()
+    expect(selfservice_portal.protect_account_tile).to_be_visible()
 
 
 @pytest.mark.selfservice
@@ -199,12 +198,12 @@ def test_admin_sees_correct_tiles_in_selfservice_portal(page, admin_username, ad
 @pytest.mark.development_environment
 @pytest.mark.acceptance_environment
 def test_admin_does_not_see_umc_tiles_in_selfservice_portal(page, admin_username, admin_password):
-    selfservice_portal_logged_in = SelfservicePortalLoggedIn(page)
-    selfservice_portal_logged_in.navigate()
-    selfservice_portal_logged_in.login(admin_username, admin_password)
+    selfservice_portal = SelfservicePortal(page)
+    selfservice_portal.navigate()
+    selfservice_portal.login(admin_username, admin_password)
 
-    expect(selfservice_portal_logged_in.tiles.first).to_be_visible()
-    expect(selfservice_portal_logged_in.umc_tiles_headline).not_to_be_visible()
+    expect(selfservice_portal.tiles.first).to_be_visible()
+    expect(selfservice_portal.umc_tiles_headline).not_to_be_visible()
 
 
 @pytest.mark.selfservice
@@ -215,12 +214,12 @@ def test_user_sees_correct_tiles_in_selfservice_portal(page, user, user_password
     username = user.properties["username"]
     wait_for_portal_sync(username, 4)
 
-    selfservice_portal_logged_in = SelfservicePortalLoggedIn(page)
-    selfservice_portal_logged_in.navigate()
-    selfservice_portal_logged_in.login(username, user_password)
+    selfservice_portal = SelfservicePortal(page)
+    selfservice_portal.navigate()
+    selfservice_portal.login(username, user_password)
 
-    expect(selfservice_portal_logged_in.my_profile_tile).to_be_visible()
-    expect(selfservice_portal_logged_in.protect_account_tile).to_be_visible()
+    expect(selfservice_portal.my_profile_tile).to_be_visible()
+    expect(selfservice_portal.protect_account_tile).to_be_visible()
 
 
 @pytest.mark.selfservice
