26730bb55bb256e02b8ada7f878d63f088bb487d
Johannes Bornhold <johannes.bornhold.extern@univention.de>
2024-11-28 13:56:18 +0100

    feat: Add fixtures to configure the Kubernetes client

---
+++ tests/ldap_chaos/conftest.py
@@ -0,0 +1,28 @@
+# SPDX-License-Identifier: AGPL-3.0-only
+# SPDX-FileCopyrightText: 2024 Univention GmbH
+
+import pytest
+from kubernetes import config
+
+
+@pytest.fixture(autouse=True, scope="session")
+def k8s_configure_client():
+    """
+    Configures the Kubernetes client.
+
+    Does not (yet) support in-cluster configuration.
+    """
+    config.load_kube_config()
+
+
+@pytest.fixture(scope="session")
+def k8s_namespace():
+    """
+    The Kubernetes Namespace to use when interacting with the cluster.
+
+    The namespace is selected from the currently active context. Inspect and
+    prepare via ``kubens``.
+    """
+    _, active_context = config.list_kube_config_contexts()
+    namespace = active_context["context"]["namespace"]
+    return namespace
