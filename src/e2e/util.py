8fce5c0368efef82fdde071e92466b31ea6e08de
Johannes Bornhold <johannes.bornhold.extern@univention.de>
2024-12-17 14:13:15 +0100

    refactor: Move StoppableAsyncThread into the module e2e.util

--- src/e2e/util.py
+++ src/e2e/util.py
@@ -1,4 +1,7 @@
+import asyncio
+import contextlib
 import logging
+import threading
 import time
 
 
@@ -13,3 +16,43 @@ def wait_until(func, expected, timeout=10):
         time.sleep(1)
     else:
         raise Exception("Timed out in wait_until.")
+
+
+class StoppableAsyncThread(threading.Thread):
+    """
+    Utility to run async code in a thread in the background.
+
+    Either override the method `arun` with your own implementation or
+    provide the keyword argument `atarget`.
+
+    The method `stop` will raise a `CancelledError` in the running `atarget`.
+    """
+
+    def __init__(self, *, atarget=None):
+        super().__init__()
+        self._atarget = atarget
+        self._should_stop = threading.Event()
+
+    def run(self):
+        asyncio.run(self._run_arun_until_stopped())
+
+    async def _run_arun_until_stopped(self):
+        task_atarget = asyncio.create_task(self.arun())
+
+        while not self.stopped():
+            await asyncio.sleep(0.1)
+
+        task_atarget.cancel()
+        with contextlib.suppress(asyncio.CancelledError):
+            await task_atarget
+
+    async def arun(self):
+        if not self._atarget:
+            return
+        await self._atarget
+
+    def stop(self):
+        self._should_stop.set()
+
+    def stopped(self):
+        return self._should_stop.is_set()
--- tests-chaos/ldap_chaos/test_provisioning.py
+++ tests-chaos/ldap_chaos/test_provisioning.py
@@ -1,8 +1,6 @@
-import asyncio
 import contextlib
 import logging
 import queue
-import threading
 import time
 
 from ldap3 import MODIFY_REPLACE
@@ -10,7 +8,7 @@ from univention.provisioning.consumer import ProvisioningConsumerClient, Provisi
 from univention.provisioning.models import RealmTopic, MessageProcessingStatus
 import pytest
 
-from e2e.util import wait_until
+from e2e.util import StoppableAsyncThread, wait_until
 
 
 log = logging.getLogger(__name__)
@@ -64,46 +62,6 @@ async def users_consumer(messages: queue.Queue):
                     subscription_name, response.sequence_number, MessageProcessingStatus.ok
                 )
                 messages.put(response, timeout=1)
-
-
-class StoppableAsyncThread(threading.Thread):
-    """
-    Utility to run async code in a thread in the background.
-
-    Either override the method `arun` with your own implementation or
-    provide the keyword argument `atarget`.
-
-    The method `stop` will raise a `CancelledError` in the running `atarget`.
-    """
-
-    def __init__(self, *, atarget=None):
-        super().__init__()
-        self._atarget = atarget
-        self._should_stop = threading.Event()
-
-    def run(self):
-        asyncio.run(self._run_arun_until_stopped())
-
-    async def _run_arun_until_stopped(self):
-        task_atarget = asyncio.create_task(self.arun())
-
-        while not self.stopped():
-            await asyncio.sleep(0.1)
-
-        task_atarget.cancel()
-        with contextlib.suppress(asyncio.CancelledError):
-            await task_atarget
-
-    async def arun(self):
-        if not self._atarget:
-            return
-        await self._atarget
-
-    def stop(self):
-        self._should_stop.set()
-
-    def stopped(self):
-        return self._should_stop.is_set()
 
 
 @pytest.fixture
