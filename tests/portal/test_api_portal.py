8eddf2ff362d988f6008bc707bb980f99c07a172
Johannes Bornhold <johannes.bornhold.extern@univention.de>
2025-03-11 08:21:09 +0100

    feat: Check link lists in the portal API

--- tests/portal/test_api_portal.py
+++ tests/portal/test_api_portal.py
@@ -4,6 +4,7 @@
 import pytest
 
 from e2e.api.portal_server import PortalServerApi
+from e2e.decorators import retrying_fast
 from umspages.portal.home_page.base import HomePage
 
 
@@ -16,9 +17,27 @@ from umspages.portal.home_page.base import HomePage
         "user_links",
     ],
 )
-def test_result_contains_quick_links(page, portal_link_list):
+def test_result_contains_links(page, portal_link_list):
     home_page = HomePage(page)
     home_page.navigate()
     api = PortalServerApi(page.request)
     result = api.get_portal().json()
     assert portal_link_list in result
+
+
+def test_added_entry_appears_in_links(page, udm, ldap_base_dn, portal_entry, portal_link_list):
+    portal_module = udm.get("portals/portal")
+    default_portal = portal_module.get(f"cn=domain,cn=portal,cn=portals,cn=univention,{ldap_base_dn}")
+    default_portal.properties[portal_link_list.udm_attr].append(portal_entry.dn)
+    default_portal.save()
+
+    home_page = HomePage(page)
+    home_page.navigate()
+    api = PortalServerApi(page.request)
+
+    @retrying_fast
+    def assert_entry_is_in_link_list():
+        result = api.get_portal().json()
+        assert portal_entry.dn in result[portal_link_list.portal_attr]
+
+    assert_entry_is_in_link_list()
