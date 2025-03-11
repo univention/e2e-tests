166adc2f8d3d5737167cfb0d4fef9c34563feb86
Johannes Bornhold <johannes.bornhold.extern@univention.de>
2025-03-11 12:18:15 +0100

    refactor: Consolidate the portal update test to leverage existing infrastructure
    - Using the existing "retry" mechanism.
    - Using the existing fixtures.

--- src/e2e/api/portal_server.py
+++ src/e2e/api/portal_server.py
@@ -11,7 +11,7 @@ from e2e.types import LdapDn
 
 class PortalServerApi:
     """
-    A model to test the portal server API.
+    A model to test the portal server API from the Browser.
     """
 
     base_path = "/univention/portal/api/v1/"
@@ -33,7 +33,6 @@ class PortalServerApi:
         """
         return self.get("../../portal.json")
 
-
     @retrying_fast
     def assert_item_is_in_link_list(self, item_dn: LdapDn, link_list: str) -> None:
         """
@@ -44,3 +43,15 @@ class PortalServerApi:
         """
         result = self.get_portal().json()
         assert item_dn in result[link_list]
+
+    @retrying_fast
+    def assert_entry(self, entry_dn: LdapDn) -> None:
+        """
+        Check that an `entry_dn` is present in the portal response.
+
+        The method is retrying to allow for the consumer to update the portal
+        cache.
+        """
+        result = self.get_portal().json()
+        included_entries = {entry["dn"] for entry in result["entries"]}
+        assert entry_dn in included_entries
--- tests/portal/test_api_portal.py
+++ tests/portal/test_api_portal.py
@@ -1,14 +1,17 @@
 # SPDX-License-Identifier: AGPL-3.0-only
 # SPDX-FileCopyrightText: 2025 Univention GmbH
 
-import time
-from urllib.parse import urljoin
-
 import pytest
-import requests
 
 from e2e.api.portal_server import PortalServerApi
 from univention.admin.rest.client import UDM
+
+
+pytestmark = [
+    pytest.mark.portal,
+    pytest.mark.development_environment,
+    pytest.mark.acceptance_environment,
+]
 
 
 def test_result_contains_links(page, portal_link_list):
@@ -45,43 +48,12 @@ def _add_item_into_link_list(udm, ldap_base_dn, portal_link_list, item_dn):
     default_portal.save()
 
 
-@pytest.fixture
-def portal_entry(udm, ldap_base_dn, portal_entry):
-    """Portal Entry in a Portal Category."""
+def test_create_portal_entry(page, udm: UDM, ldap_base_dn, portal_entry):
+    api = PortalServerApi(page.request)
     # Add portal_entry to the portal categories to make it visible and accessible through the URL
     # f"{portal_endpoint}/univention/portal/portal.json"
     category = udm.obj_by_dn(f"cn=domain-service,cn=category,cn=portals,cn=univention,{ldap_base_dn}")
     category.properties["entries"].append(portal_entry.dn)
     category.save()
-    return portal_entry
 
-
-@pytest.fixture
-def portal_api_url(portal_base_url):
-    """URL of the portal API in the Portal."""
-    return urljoin(portal_base_url, "/univention/portal/portal.json")
-
-
-@pytest.fixture()
-def wait_for_portal_update(udm: UDM, portal_base_url: str, portal_api_url):
-    def _wait_for_entry(entry_dn: str, timeout=30.0) -> bool:
-        start_time = time.time()
-        while time.time() - start_time < timeout:
-            response = requests.get(portal_api_url)
-            assert response.status_code == 200
-
-            entries = response.json()["entries"]
-            if any(entry.get("dn") == entry_dn for entry in entries):
-                return True
-            time.sleep(0.1)
-        return False
-
-    return _wait_for_entry
-
-
-@pytest.mark.portal
-@pytest.mark.development_environment
-@pytest.mark.acceptance_environment
-def test_create_portal_entry(portal_entry, wait_for_portal_update):
-    entry_exists = wait_for_portal_update(portal_entry.dn)
-    assert entry_exists
+    api.assert_entry(portal_entry.dn)
