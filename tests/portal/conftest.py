cb6e34d0c6b93aa76503e6dd79a8b47829786905
Jan Burgmeier <jan.burgmeier.extern@univention.de>
2025-06-02 10:43:07 +0200

    fix(portal-tests): Increase ensure_user_exists timeout to 30s
    univention/dev/internal/team-nubus#1223

--- tests/portal/conftest.py
+++ tests/portal/conftest.py
@@ -249,7 +249,7 @@ def ensure_user_exists(minio_client) -> WaitForUserExists:
     Allows to wait until the portal data for a user is complete.
     """
 
-    def _wait_for_user_creation(username: str, timeout: int | float = 10) -> None:
+    def _wait_for_user_creation(username: str, timeout: int | float = 30) -> None:
         @retry(
             stop=stop_after_delay(timeout),
             wait=wait_fixed(0.25),
